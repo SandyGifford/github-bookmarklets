bb7d9ccc8472a5aa3b5d676ae610786c6a58ee93
Sandy <sanford.r.gifford@gmail.com>
2024-04-16 14:07:27 -0400

    conditionally follow tracking code

--- src/bookmarklets/Impersonate/index.ts
+++ src/bookmarklets/Impersonate/index.ts
@@ -1,2 +1,3 @@
 const trackingCode = prompt("Tracking Code");
-location.href = `https://app.reclaim.ai/?shadowTrackingCode=${trackingCode}`;
+if (trackingCode)
+  location.href = `https://app.reclaim.ai/?shadowTrackingCode=${trackingCode}`;
